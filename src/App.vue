964bb8eaeed849ccf488c390ba4d8417d67a8b33
HWB0104 <a2801614708@gmail.com>
2023-12-06 22:27:27 +0800

    tjiao

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  * @Author: HWB0104 a2801614708@gmail.com
  * @Date: 2023-12-06 22:15:23
  * @LastEditors: HWB0104 a2801614708@gmail.com
- * @LastEditTime: 2023-12-06 22:20:08
+ * @LastEditTime: 2023-12-06 22:25:55
  * @FilePath: \sy\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -10,7 +10,7 @@
   <div id="app">
 
     <!-- 路由站位符 -->
-    <div class="aa">黄伟博提交</div>
+    <div class="aa">黄伟博提交2222</div>
     <router-view></router-view>
   </div>
 </template>
